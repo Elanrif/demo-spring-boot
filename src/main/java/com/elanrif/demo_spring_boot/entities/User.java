7ab188d93fbd90a35fc35dab0ab69391b3cdbc35
Elanrif <saidbacoelanrif@gmail.com>
2025-09-17 19:17:44 +0100

    feat: User entity

---
+++ src/main/java/com/elanrif/demo_spring_boot/entities/User.java
@@ -0,0 +1,34 @@
+package com.elanrif.demo_spring_boot.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String phone;
+
+    private String address;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
