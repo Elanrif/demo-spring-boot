c67ab0b94b72bf97d4e74393ef9910ab4d4bf577
Elanrif <saidbacoelanrif@gmail.com>
2025-10-27 16:04:11 +0100

    chore: authentication jwt

--- src/main/java/com/elanrif/inventory_management/security/WebSecurityConfig.java
+++ src/main/java/com/elanrif/inventory_management/security/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig {
                 .exceptionHandling(exception  -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/api/auth/**").permitAll()
+                        auth.requestMatchers("/api/auth/**","/api/products/**").permitAll()
                                 .anyRequest().authenticated()
                         );
         http.authenticationProvider(authenticationProvider());
