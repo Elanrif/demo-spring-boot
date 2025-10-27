93012c36ecd24bb47d953a72175d7d85d7cd192c
Elanrif <saidbacoelanrif@gmail.com>
2025-10-27 13:34:15 +0100

    fix: loadUsername by password

--- src/main/java/com/elanrif/inventory_management/payload/request/LoginRequest.java
+++ src/main/java/com/elanrif/inventory_management/payload/request/LoginRequest.java
@@ -8,9 +8,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequest {
-    @NotBlank
-    private String username;
 
     @NotBlank
     private String password;
+
+    @NotBlank
+    private String email;
 }
