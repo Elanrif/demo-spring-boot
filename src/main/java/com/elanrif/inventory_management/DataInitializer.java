a7c7a1371bda00eb74292f2c0d0b7267de6db233
Elanrif <saidbacoelanrif@gmail.com>
2025-10-29 19:53:21 +0100

    feat: refresh token

--- src/main/java/com/elanrif/inventory_management/DataInitializer.java
+++ src/main/java/com/elanrif/inventory_management/DataInitializer.java
@@ -1,16 +1,11 @@
 package com.elanrif.inventory_management;
 
-import com.elanrif.inventory_management.entities.Category;
-import com.elanrif.inventory_management.entities.Product;
 import com.elanrif.inventory_management.entities.Role;
 import com.elanrif.inventory_management.enums.ERole;
-import com.elanrif.inventory_management.enums.CategoryEnum;
 import com.elanrif.inventory_management.payload.request.SignupRequest;
-import com.elanrif.inventory_management.repository.CategoryRepository;
-import com.elanrif.inventory_management.repository.ProductRepository;
 import com.elanrif.inventory_management.repository.RoleRepository;
 import com.elanrif.inventory_management.repository.UserRepository;
-import com.elanrif.inventory_management.services.UserService;
+import com.elanrif.inventory_management.security.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -22,18 +17,10 @@ public class DataInitializer implements CommandLineRunner {
 
     @Autowired
     private RoleRepository roleRepository;
-
-    @Autowired
-    private UserService userService;
-
     @Autowired
     private UserRepository userRepository;
-
     @Autowired
-    private CategoryRepository categoryRepository;
-
-    @Autowired
-    private ProductRepository productRepository;
+    private AuthService authService;
 
     @Override
     public void run(String... args) throws Exception {
@@ -58,27 +45,7 @@ public class DataInitializer implements CommandLineRunner {
                 .roles(Set.of("admin", "moderator"))
                 .build();
         if (userRepository.findByUsername("admin").isEmpty()) {
-            userService.signUp(adminSignupRequest);
-        }
-
-        /* Create Categories */
-        if (categoryRepository.findAll().isEmpty()) {
-            Category electronics = new Category(null, null, "Electronics", "Devices and gadgets", false, null, CategoryEnum.ACTIVE, null, null);
-            Category clothing = new Category(null, null, "Clothing", "Apparel and accessories", false, null, CategoryEnum.ACTIVE, null, null);
-            categoryRepository.save(electronics);
-            categoryRepository.save(clothing);
-        }
-
-        /* Create Products */
-        if (productRepository.findAll().isEmpty()) {
-            Category electronics = categoryRepository.findAll().get(0); // Assuming Electronics is the first category
-            Category clothing = categoryRepository.findAll().get(1); // Assuming Clothing is the second category
-
-            Product phone = new Product(null, electronics, "Smartphone", 699.99, "Latest model smartphone", 50, null, null);
-            Product tshirt = new Product(null, clothing, "T-Shirt", 19.99, "Comfortable cotton t-shirt", 200, null, null);
-
-            productRepository.save(phone);
-            productRepository.save(tshirt);
+            authService.registerAndGenerateJwt(adminSignupRequest,false);
         }
     }
 }
--- src/main/java/com/elanrif/inventory_management/controllers/AuthController.java
+++ src/main/java/com/elanrif/inventory_management/controllers/AuthController.java
@@ -1,166 +1,39 @@
 package com.elanrif.inventory_management.controllers;
 
-import com.elanrif.inventory_management.entities.RefreshToken;
-import com.elanrif.inventory_management.entities.Role;
-import com.elanrif.inventory_management.entities.User;
-import com.elanrif.inventory_management.enums.ERole;
-import com.elanrif.inventory_management.exception.TokenRefreshException;
 import com.elanrif.inventory_management.payload.request.LoginRequest;
 import com.elanrif.inventory_management.payload.request.SignupRequest;
 import com.elanrif.inventory_management.payload.request.TokenRefreshRequest;
-import com.elanrif.inventory_management.payload.response.JwtResponse;
-import com.elanrif.inventory_management.payload.response.MessageResponse;
-import com.elanrif.inventory_management.payload.response.TokenRefreshResponse;
-import com.elanrif.inventory_management.repository.RoleRepository;
-import com.elanrif.inventory_management.repository.UserRepository;
-import com.elanrif.inventory_management.security.jwt.JwtUtils;
-import com.elanrif.inventory_management.security.services.UserDetailsImpl;
-import com.elanrif.inventory_management.security.services.RefreshTokenService;
+import com.elanrif.inventory_management.security.services.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    @Autowired
-    AuthenticationManager authenticationManager;
 
     @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    RoleRepository roleRepository;
-
-    @Autowired
-    PasswordEncoder encoder;
-
-    @Autowired
-    JwtUtils jwtUtils;
-
-    @Autowired
-    RefreshTokenService refreshTokenService;
+    AuthService authService;
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword())
-        );
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-
-        String jwt = jwtUtils.generateJwtToken(userDetails);
-
-        List<String> roles = userDetails.getAuthorities().stream()
-                .map(item -> item.getAuthority())
-                .collect(Collectors.toList());
-
-        RefreshToken refreshToken = refreshTokenService.createRefreshToken(userDetails.getId());
-
-        return ResponseEntity.ok(new JwtResponse(jwt,
-                refreshToken.getToken(),
-                userDetails.getId(),
-                userDetails.getUsername(),
-                userDetails.getEmail(),
-                roles));
+        return authService.authenticateAndGenerateJwt(loginRequest);
     }
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-            return ResponseEntity
-                    .badRequest()
-                    .body(new MessageResponse("Error: Username is already taken!"));
-        }
-
-        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-            return ResponseEntity
-                    .badRequest()
-                    .body(new MessageResponse("Error: Email is already in use!"));
-        }
-
-        // Create new user's account
-        User user = User.builder()
-                .username(signUpRequest.getUsername())
-                .address(signUpRequest.getAddress())
-                .phone(signUpRequest.getPhone())
-                .email(signUpRequest.getEmail())
-                .password(encoder.encode(signUpRequest.getPassword()))
-                .build();
-
-        Set<String> strRoles = signUpRequest.getRoles();
-        Set<Role> roles = new HashSet<>();
-
-        if (strRoles == null) {
-            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-            roles.add(userRole);
-        } else {
-            strRoles.forEach(role -> {
-                switch (role) {
-                    case "admin":
-                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                        roles.add(adminRole);
-
-                        break;
-                    case "moderator":
-                        Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
-                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                        roles.add(modRole);
-
-                        break;
-                    default:
-                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                        roles.add(userRole);
-                }
-            });
-        }
-
-        user.setRoles(roles);
-        userRepository.save(user);
-
-        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+        return authService.registerAndGenerateJwt(signUpRequest);
     }
 
     @PostMapping("/refreshToken")
     public ResponseEntity<?> refreshtoken(@Valid @RequestBody TokenRefreshRequest request) {
-        String requestRefreshToken = request.getRefreshToken();
-
-        return refreshTokenService.findByToken(requestRefreshToken)
-                .map(refreshTokenService::verifyExpiration)
-                .map(RefreshToken::getUser)
-                .map(user -> {
-                    String token = jwtUtils.generateTokenFromUsername(user.getEmail());
-                    System.out.println("refreshtoken: generated new access token for user -> [" + user.getEmail() + "]");
-                    return ResponseEntity.ok(new TokenRefreshResponse(token, requestRefreshToken));
-                })
-                .orElseThrow(() -> {
-                    // log plus d'info pour debug
-                    System.err.println("refreshtoken: token not found in DB -> [" + requestRefreshToken + "]");
-                    return new TokenRefreshException(requestRefreshToken, "Refresh token is not in database!");
-                });
+        return authService.refreshToken(request.getRefreshToken());
     }
 
     @PostMapping("/logout")
     public ResponseEntity<?> logoutUser() {
-        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Long userId = userDetails.getId();
-        refreshTokenService.deleteByUserId(userId);
-        return ResponseEntity.ok(new MessageResponse("Log out successful!"));
+        return authService.logoutCurrentUser();
     }
 }
